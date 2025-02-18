e4530815404e05454ba0b867a4ed13505bb7cdd2
Tri Pham <devltt404@gmail.com>
2025-02-18 15:02:28 -0500

    Add chart and grid display

--- StockAnalysisCS/Form_AnalyzeStock.Designer.cs
+++ StockAnalysisCS/Form_AnalyzeStock.Designer.cs
@@ -28,16 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.button_loadTicker = new System.Windows.Forms.Button();
             this.openFileDialog_loadTicker = new System.Windows.Forms.OpenFileDialog();
+            this.dateTimePicker_startDate = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker_endDate = new System.Windows.Forms.DateTimePicker();
+            this.label_startDate = new System.Windows.Forms.Label();
+            this.label_endDate = new System.Windows.Forms.Label();
+            this.chart_stockData = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridView_stockData = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_stockData)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_stockData)).BeginInit();
             this.SuspendLayout();
             // 
             // button_loadTicker
             // 
-            this.button_loadTicker.Location = new System.Drawing.Point(47, 46);
-            this.button_loadTicker.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button_loadTicker.Location = new System.Drawing.Point(78, 43);
             this.button_loadTicker.Name = "button_loadTicker";
-            this.button_loadTicker.Size = new System.Drawing.Size(135, 45);
+            this.button_loadTicker.Size = new System.Drawing.Size(202, 69);
             this.button_loadTicker.TabIndex = 0;
             this.button_loadTicker.Text = "Load Ticker";
             this.button_loadTicker.UseVisualStyleBackColor = true;
@@ -47,17 +59,98 @@
             // 
             this.openFileDialog_loadTicker.DefaultExt = "csv";
             this.openFileDialog_loadTicker.FileName = "AAL-Day";
+            this.openFileDialog_loadTicker.Filter = "All Files|*.csv|Month|*-Month.csv|Day|*-Day.csv|Week|*-Week.csv";
+            this.openFileDialog_loadTicker.InitialDirectory = "D:\\@DEVLTT404\\USF\\@@@Spring2025\\COP4365\\StockAnalysisCS\\Stock Data";
+            this.openFileDialog_loadTicker.ShowReadOnly = true;
             this.openFileDialog_loadTicker.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog_loadTicker_FileOk);
+            // 
+            // dateTimePicker_startDate
+            // 
+            this.dateTimePicker_startDate.Location = new System.Drawing.Point(423, 43);
+            this.dateTimePicker_startDate.Name = "dateTimePicker_startDate";
+            this.dateTimePicker_startDate.Size = new System.Drawing.Size(272, 26);
+            this.dateTimePicker_startDate.TabIndex = 1;
+            this.dateTimePicker_startDate.ValueChanged += new System.EventHandler(this.dateTimePicker_startDate_ValueChanged);
+            // 
+            // dateTimePicker_endDate
+            // 
+            this.dateTimePicker_endDate.Location = new System.Drawing.Point(423, 103);
+            this.dateTimePicker_endDate.Name = "dateTimePicker_endDate";
+            this.dateTimePicker_endDate.Size = new System.Drawing.Size(272, 26);
+            this.dateTimePicker_endDate.TabIndex = 2;
+            this.dateTimePicker_endDate.ValueChanged += new System.EventHandler(this.dateTimePicker_endDate_ValueChanged);
+            // 
+            // label_startDate
+            // 
+            this.label_startDate.AutoSize = true;
+            this.label_startDate.Location = new System.Drawing.Point(308, 43);
+            this.label_startDate.Name = "label_startDate";
+            this.label_startDate.Size = new System.Drawing.Size(83, 20);
+            this.label_startDate.TabIndex = 3;
+            this.label_startDate.Text = "Start Date";
+            // 
+            // label_endDate
+            // 
+            this.label_endDate.AutoSize = true;
+            this.label_endDate.Location = new System.Drawing.Point(312, 94);
+            this.label_endDate.Name = "label_endDate";
+            this.label_endDate.Size = new System.Drawing.Size(77, 20);
+            this.label_endDate.TabIndex = 4;
+            this.label_endDate.Text = "End Date";
+            // 
+            // chart_stockData
+            // 
+            chartArea3.Name = "ChartArea_OHLC";
+            chartArea4.Name = "ChartArea_Volume";
+            this.chart_stockData.ChartAreas.Add(chartArea3);
+            this.chart_stockData.ChartAreas.Add(chartArea4);
+            legend2.Name = "Legend1";
+            this.chart_stockData.Legends.Add(legend2);
+            this.chart_stockData.Location = new System.Drawing.Point(78, 244);
+            this.chart_stockData.Name = "chart_stockData";
+            series3.ChartArea = "ChartArea_OHLC";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Candlestick;
+            series3.Legend = "Legend1";
+            series3.Name = "Series_OHLC";
+            series3.YValuesPerPoint = 4;
+            series4.ChartArea = "ChartArea_Volume";
+            series4.Legend = "Legend1";
+            series4.Name = "Series_Volume";
+            this.chart_stockData.Series.Add(series3);
+            this.chart_stockData.Series.Add(series4);
+            this.chart_stockData.Size = new System.Drawing.Size(1051, 417);
+            this.chart_stockData.TabIndex = 5;
+            this.chart_stockData.Text = "chart_stockData";
+            // 
+            // dataGridView_stockData
+            // 
+            this.dataGridView_stockData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_stockData.Location = new System.Drawing.Point(781, 43);
+            this.dataGridView_stockData.Name = "dataGridView_stockData";
+            this.dataGridView_stockData.RowHeadersWidth = 62;
+            this.dataGridView_stockData.RowTemplate.Height = 28;
+            this.dataGridView_stockData.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView_stockData.TabIndex = 6;
             // 
             // Form_AnalyzeStock
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.dataGridView_stockData);
+            this.Controls.Add(this.chart_stockData);
+            this.Controls.Add(this.label_endDate);
+            this.Controls.Add(this.label_startDate);
+            this.Controls.Add(this.dateTimePicker_endDate);
+            this.Controls.Add(this.dateTimePicker_startDate);
             this.Controls.Add(this.button_loadTicker);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form_AnalyzeStock";
             this.Text = "Form_AnalyzeStock";
+            ((System.ComponentModel.ISupportInitialize)(this.chart_stockData)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_stockData)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -65,6 +158,12 @@
 
         private System.Windows.Forms.Button button_loadTicker;
         private System.Windows.Forms.OpenFileDialog openFileDialog_loadTicker;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_startDate;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_endDate;
+        private System.Windows.Forms.Label label_startDate;
+        private System.Windows.Forms.Label label_endDate;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart_stockData;
+        private System.Windows.Forms.DataGridView dataGridView_stockData;
     }
 }
 
--- StockAnalysisCS/Form_AnalyzeStock.cs
+++ StockAnalysisCS/Form_AnalyzeStock.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace StockAnalysisCS
 {
     public partial class Form_AnalyzeStock : Form
     {
+        // Declare a list to store Candlestick objects
+        private List<Candlestick> candlesticks = null;
+        // Declare a binding list to store filtered candlestick objects
+        private BindingList<Candlestick> filteredCandlesticks = null;
+
         public Form_AnalyzeStock()
         {
             InitializeComponent();
+            // Initialize the candlesticks list
+            candlesticks = new List<Candlestick>();
         }
 
         /// <summary>
@@ -35,7 +45,152 @@ namespace StockAnalysisCS
         /// <param name="e"></param>
         private void openFileDialog_loadTicker_FileOk(object sender, CancelEventArgs e)
         {
+            // Set the form Text to the selected file name
+            Text = openFileDialog_loadTicker.FileName;
+            readCandlesticksFromFile();
+            filterCandlesticks();
+            normalizeChart();
+            displayChart();
+            displayDataGridView();
+        }
 
+        /// <summary>
+        /// Function to read candlestick data from the selected file and store it in a list
+        /// </summary>
+        private void readCandlesticksFromFile()
+        {
+            // Get the file name selected in the OpenFileDialog
+            string fileName = openFileDialog_loadTicker.FileName;
+
+            // Declare the expected header row (first line) of the CSV file
+            const string expectedHeader = "\"Date\",\"Open\",\"High\",\"Low\",\"Close\",\"Volume\"";
+
+            // Open the file to read its contents
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                // Read the first line of the file
+                string line = sr.ReadLine();
+
+                // Check if the first line matches the expected header
+                if (line != expectedHeader)
+                {
+                    // Show an error message if the header is invalid
+                    MessageBox.Show("Invalid file format.");
+                    // Exit function
+                    return;
+                }
+
+                // Clear the existing elements in the candlesticks list
+                candlesticks.Clear();
+
+                // Read the file util reaching the end
+                while (!sr.EndOfStream)
+                {
+                    // Read a line from the file
+                    line = sr.ReadLine();
+                    // Create a new Candlestick object from the line
+                    Candlestick candlestick = new Candlestick(line);
+                    // Add the Candlestick object to the list
+                    candlesticks.Add(candlestick);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Function to filter the candlestick data based on user-selected date range
+        /// </summary>
+        private void filterCandlesticks()
+        {
+            // Get the user-selected start date
+            DateTime startDate = dateTimePicker_startDate.Value;
+            // Get the user-selected end date
+            DateTime endDate = dateTimePicker_endDate.Value;
+
+            // Use LINQ to filter candlesticks within the date range
+            // and assign the result to the filteredCandlesticks list
+            filteredCandlesticks = new BindingList<Candlestick>(candlesticks.Where(c => c.date >= startDate && c.date <= endDate).ToList());
+        }
+
+        /// <summary>
+        /// Function to normalize the y-axis of the chart 
+        /// to fit the candlesticks data for better visualization
+        /// </summary>
+        private void normalizeChart()
+        {
+            // Check if there are no candlesticks within the date range to display
+            if (filteredCandlesticks == null || filteredCandlesticks.Count == 0)
+            {
+                // Exit function
+                return;
+            }
+            // Find the minimum low price in the filtered candlesticks
+            decimal minLow = filteredCandlesticks.Min(c => c.low);
+            // Find the maximum high price in the filtered candlesticks
+            decimal maxHigh = filteredCandlesticks.Max(c => c.high);
+
+            // Calculate the 2% margin
+            decimal margin = (maxHigh - minLow) * 0.02m;
+
+            // Set the minimum value of the y-axis for the OHLC chart area
+            // to the minimum low price minus the margin
+            chart_stockData.ChartAreas["ChartArea_OHLC"].AxisY.Minimum = (double)(minLow - margin);
+            // Set the maximum value of the y-axis for the OHLC chart area
+            // to the maximum high price plus the margin
+            chart_stockData.ChartAreas["ChartArea_OHLC"].AxisY.Maximum = (double)(maxHigh + margin);
+        }
+
+        /// <summary>
+        /// Function to display the OHLCV stock data in the chart
+        /// </summary>
+        private void displayChart()
+        {
+            // Clear existing data in the chart
+            chart_stockData.Series["Series_OHLC"].Points.Clear();
+            chart_stockData.Series["Series_Volume"].Points.Clear();
+
+            // Loop through each candlestick in the filtered list
+            foreach (var candle in filteredCandlesticks)
+            {
+                // Add a data point to the "Series_OHLC" series (X: date, Y: open, high, low, close)
+                int pointIdx = chart_stockData.Series["Series_OHLC"].Points.AddXY(candle.date, candle.open, candle.high, candle.low, candle.close);
+
+                // Set the color for the candlestick
+                // Green for up candlestick (close >= open), Red for down candlestick (close < open)
+                chart_stockData.Series["Series_OHLC"].Points[pointIdx].Color = (candle.close >= candle.open) ? Color.Green : Color.Red;
+
+                // Add a data point to the "Series_Volume" series (X: date, Y: volume)
+                chart_stockData.Series["Series_Volume"].Points.AddXY(candle.date, candle.volume);
+            }
+        }
+
+        /// <summary>
+        /// Function to display the stock data (OHLCV) in a DataGridView
+        /// </summary>
+        private void displayDataGridView()
+        {
+            dataGridView_stockData.DataSource = filteredCandlesticks;
+        }
+
+        /// <summary>
+        /// Function to handle the event when the user selects a date in the start date picker
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dateTimePicker_startDate_ValueChanged(object sender, EventArgs e)
+        {
+            // Filter the candlesticks based on the new specified date range
+            filterCandlesticks();
+        }
+
+        /// <summary>
+        /// Function to handle the event when the user selects a date in the end date picker
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dateTimePicker_endDate_ValueChanged(object sender, EventArgs e)
+        {
+            // Filter the candlesticks based on the new specified date range
+            filterCandlesticks();
         }
     }
 }
