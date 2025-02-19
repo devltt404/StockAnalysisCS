d3a2676375dc382ec9248784fa7cd555a5df8426
Tri Pham <devltt404@gmail.com>
2025-02-19 13:57:38 -0500

    add comboboxes

--- StockAnalysisCS/Form_AnalyzeStock.Designer.cs
+++ StockAnalysisCS/Form_AnalyzeStock.Designer.cs
@@ -28,11 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.button_loadTicker = new System.Windows.Forms.Button();
             this.openFileDialog_loadTicker = new System.Windows.Forms.OpenFileDialog();
             this.dateTimePicker_startDate = new System.Windows.Forms.DateTimePicker();
@@ -41,18 +41,26 @@
             this.label_endDate = new System.Windows.Forms.Label();
             this.chart_stockData = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.dataGridView_stockData = new System.Windows.Forms.DataGridView();
+            this.comboBox_symbol = new System.Windows.Forms.ComboBox();
+            this.label_symbol = new System.Windows.Forms.Label();
+            this.comboBox_period = new System.Windows.Forms.ComboBox();
+            this.label_period = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart_stockData)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView_stockData)).BeginInit();
             this.SuspendLayout();
             // 
             // button_loadTicker
             // 
-            this.button_loadTicker.Location = new System.Drawing.Point(78, 43);
+            this.button_loadTicker.BackColor = System.Drawing.Color.MediumBlue;
+            this.button_loadTicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_loadTicker.ForeColor = System.Drawing.Color.White;
+            this.button_loadTicker.Location = new System.Drawing.Point(34, 14);
+            this.button_loadTicker.Margin = new System.Windows.Forms.Padding(2);
             this.button_loadTicker.Name = "button_loadTicker";
-            this.button_loadTicker.Size = new System.Drawing.Size(202, 69);
+            this.button_loadTicker.Size = new System.Drawing.Size(135, 45);
             this.button_loadTicker.TabIndex = 0;
             this.button_loadTicker.Text = "Load Ticker";
-            this.button_loadTicker.UseVisualStyleBackColor = true;
+            this.button_loadTicker.UseVisualStyleBackColor = false;
             this.button_loadTicker.Click += new System.EventHandler(this.button_loadTicker_Click);
             // 
             // openFileDialog_loadTicker
@@ -66,77 +74,160 @@
             // 
             // dateTimePicker_startDate
             // 
-            this.dateTimePicker_startDate.Location = new System.Drawing.Point(423, 43);
+            this.dateTimePicker_startDate.Location = new System.Drawing.Point(97, 69);
+            this.dateTimePicker_startDate.Margin = new System.Windows.Forms.Padding(2);
             this.dateTimePicker_startDate.Name = "dateTimePicker_startDate";
-            this.dateTimePicker_startDate.Size = new System.Drawing.Size(272, 26);
+            this.dateTimePicker_startDate.Size = new System.Drawing.Size(183, 20);
             this.dateTimePicker_startDate.TabIndex = 1;
             this.dateTimePicker_startDate.ValueChanged += new System.EventHandler(this.dateTimePicker_startDate_ValueChanged);
             // 
             // dateTimePicker_endDate
             // 
-            this.dateTimePicker_endDate.Location = new System.Drawing.Point(423, 103);
+            this.dateTimePicker_endDate.Location = new System.Drawing.Point(97, 102);
+            this.dateTimePicker_endDate.Margin = new System.Windows.Forms.Padding(2);
             this.dateTimePicker_endDate.Name = "dateTimePicker_endDate";
-            this.dateTimePicker_endDate.Size = new System.Drawing.Size(272, 26);
+            this.dateTimePicker_endDate.Size = new System.Drawing.Size(183, 20);
             this.dateTimePicker_endDate.TabIndex = 2;
             this.dateTimePicker_endDate.ValueChanged += new System.EventHandler(this.dateTimePicker_endDate_ValueChanged);
             // 
             // label_startDate
             // 
             this.label_startDate.AutoSize = true;
-            this.label_startDate.Location = new System.Drawing.Point(308, 43);
+            this.label_startDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_startDate.ForeColor = System.Drawing.Color.Black;
+            this.label_startDate.Location = new System.Drawing.Point(32, 71);
+            this.label_startDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label_startDate.Name = "label_startDate";
-            this.label_startDate.Size = new System.Drawing.Size(83, 20);
+            this.label_startDate.Size = new System.Drawing.Size(61, 15);
             this.label_startDate.TabIndex = 3;
             this.label_startDate.Text = "Start Date";
             // 
             // label_endDate
             // 
             this.label_endDate.AutoSize = true;
-            this.label_endDate.Location = new System.Drawing.Point(312, 94);
+            this.label_endDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_endDate.ForeColor = System.Drawing.Color.Black;
+            this.label_endDate.Location = new System.Drawing.Point(32, 103);
+            this.label_endDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label_endDate.Name = "label_endDate";
-            this.label_endDate.Size = new System.Drawing.Size(77, 20);
+            this.label_endDate.Size = new System.Drawing.Size(58, 15);
             this.label_endDate.TabIndex = 4;
             this.label_endDate.Text = "End Date";
             // 
             // chart_stockData
             // 
-            chartArea3.Name = "ChartArea_OHLC";
-            chartArea4.Name = "ChartArea_Volume";
-            this.chart_stockData.ChartAreas.Add(chartArea3);
-            this.chart_stockData.ChartAreas.Add(chartArea4);
-            legend2.Name = "Legend1";
-            this.chart_stockData.Legends.Add(legend2);
-            this.chart_stockData.Location = new System.Drawing.Point(78, 244);
+            chartArea1.Name = "ChartArea_OHLC";
+            chartArea2.Name = "ChartArea_Volume";
+            this.chart_stockData.ChartAreas.Add(chartArea1);
+            this.chart_stockData.ChartAreas.Add(chartArea2);
+            legend1.Name = "Legend1";
+            this.chart_stockData.Legends.Add(legend1);
+            this.chart_stockData.Location = new System.Drawing.Point(35, 196);
+            this.chart_stockData.Margin = new System.Windows.Forms.Padding(2);
             this.chart_stockData.Name = "chart_stockData";
-            series3.ChartArea = "ChartArea_OHLC";
-            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Candlestick;
-            series3.Legend = "Legend1";
-            series3.Name = "Series_OHLC";
-            series3.YValuesPerPoint = 4;
-            series4.ChartArea = "ChartArea_Volume";
-            series4.Legend = "Legend1";
-            series4.Name = "Series_Volume";
-            this.chart_stockData.Series.Add(series3);
-            this.chart_stockData.Series.Add(series4);
-            this.chart_stockData.Size = new System.Drawing.Size(1051, 417);
+            series1.ChartArea = "ChartArea_OHLC";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Candlestick;
+            series1.CustomProperties = "PriceDownColor=Red, PriceUpColor=Green";
+            series1.Legend = "Legend1";
+            series1.Name = "Series_OHLC";
+            series1.XValueMember = "Date";
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Date;
+            series1.YValueMembers = "High,Low,Open,Close";
+            series1.YValuesPerPoint = 4;
+            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.UInt64;
+            series2.ChartArea = "ChartArea_Volume";
+            series2.Legend = "Legend1";
+            series2.Name = "Series_Volume";
+            series2.XValueMember = "Date";
+            series2.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Date;
+            series2.YValueMembers = "Volume";
+            series2.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.UInt64;
+            this.chart_stockData.Series.Add(series1);
+            this.chart_stockData.Series.Add(series2);
+            this.chart_stockData.Size = new System.Drawing.Size(1017, 271);
             this.chart_stockData.TabIndex = 5;
             this.chart_stockData.Text = "chart_stockData";
             // 
             // dataGridView_stockData
             // 
             this.dataGridView_stockData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView_stockData.Location = new System.Drawing.Point(781, 43);
+            this.dataGridView_stockData.Location = new System.Drawing.Point(377, 28);
+            this.dataGridView_stockData.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView_stockData.Name = "dataGridView_stockData";
             this.dataGridView_stockData.RowHeadersWidth = 62;
             this.dataGridView_stockData.RowTemplate.Height = 28;
-            this.dataGridView_stockData.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView_stockData.Size = new System.Drawing.Size(675, 150);
             this.dataGridView_stockData.TabIndex = 6;
+            // 
+            // comboBox_symbol
+            // 
+            this.comboBox_symbol.FormattingEnabled = true;
+            this.comboBox_symbol.Items.AddRange(new object[] {
+            "All",
+            "AAL",
+            "AAPL",
+            "ABBV",
+            "IBM",
+            "INTC",
+            "JPM",
+            "LUV",
+            "MSFT",
+            "WST",
+            "WTW",
+            "WY",
+            "WYNN",
+            "XEL",
+            "XOM",
+            "XRAY",
+            "XYL",
+            "YUM"});
+            this.comboBox_symbol.Location = new System.Drawing.Point(97, 133);
+            this.comboBox_symbol.Name = "comboBox_symbol";
+            this.comboBox_symbol.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_symbol.TabIndex = 7;
+            // 
+            // label_symbol
+            // 
+            this.label_symbol.AutoSize = true;
+            this.label_symbol.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_symbol.Location = new System.Drawing.Point(32, 133);
+            this.label_symbol.Name = "label_symbol";
+            this.label_symbol.Size = new System.Drawing.Size(48, 15);
+            this.label_symbol.TabIndex = 8;
+            this.label_symbol.Text = "Symbol";
+            // 
+            // comboBox_period
+            // 
+            this.comboBox_period.FormattingEnabled = true;
+            this.comboBox_period.Items.AddRange(new object[] {
+            "All",
+            "Daily",
+            "Weekly",
+            "Monthly"});
+            this.comboBox_period.Location = new System.Drawing.Point(97, 163);
+            this.comboBox_period.Name = "comboBox_period";
+            this.comboBox_period.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_period.TabIndex = 9;
+            // 
+            // label_period
+            // 
+            this.label_period.AutoSize = true;
+            this.label_period.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_period.Location = new System.Drawing.Point(31, 163);
+            this.label_period.Name = "label_period";
+            this.label_period.Size = new System.Drawing.Size(43, 15);
+            this.label_period.TabIndex = 10;
+            this.label_period.Text = "Period";
             // 
             // Form_AnalyzeStock
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.ClientSize = new System.Drawing.Size(1086, 496);
+            this.Controls.Add(this.label_period);
+            this.Controls.Add(this.comboBox_period);
+            this.Controls.Add(this.label_symbol);
+            this.Controls.Add(this.comboBox_symbol);
             this.Controls.Add(this.dataGridView_stockData);
             this.Controls.Add(this.chart_stockData);
             this.Controls.Add(this.label_endDate);
@@ -144,9 +235,8 @@
             this.Controls.Add(this.dateTimePicker_endDate);
             this.Controls.Add(this.dateTimePicker_startDate);
             this.Controls.Add(this.button_loadTicker);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form_AnalyzeStock";
-            this.Text = "Form_AnalyzeStock";
+            this.Text = "Analyze Stock";
             ((System.ComponentModel.ISupportInitialize)(this.chart_stockData)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView_stockData)).EndInit();
             this.ResumeLayout(false);
@@ -164,6 +254,10 @@
         private System.Windows.Forms.Label label_endDate;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart_stockData;
         private System.Windows.Forms.DataGridView dataGridView_stockData;
+        private System.Windows.Forms.ComboBox comboBox_symbol;
+        private System.Windows.Forms.Label label_symbol;
+        private System.Windows.Forms.ComboBox comboBox_period;
+        private System.Windows.Forms.Label label_period;
     }
 }
 
--- StockAnalysisCS/Form_AnalyzeStock.cs
+++ StockAnalysisCS/Form_AnalyzeStock.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,14 +18,24 @@ namespace StockAnalysisCS
     {
         // Declare a list to store Candlestick objects
         private List<Candlestick> candlesticks = null;
-        // Declare a binding list to store filtered candlestick objects
-        private BindingList<Candlestick> filteredCandlesticks = null;
+        // Declare a list to store filtered candlestick objects
+        private List<Candlestick> filteredCandlesticks = null;
 
         public Form_AnalyzeStock()
         {
             InitializeComponent();
             // Initialize the candlesticks list
             candlesticks = new List<Candlestick>();
+            // Initialize the filtered candlesticks list
+            filteredCandlesticks = new List<Candlestick>();
+            // Set the end date of the date picker to the current date
+            dateTimePicker_endDate.Value = DateTime.Now;
+            // Set the start date of the date picker to 2 years before the current date
+            dateTimePicker_startDate.Value = DateTime.Now.Date.AddYears(-2);
+            // Set the default selected index of the symbol combo box to 0 (All)
+            comboBox_symbol.SelectedIndex = 0;
+            // Set the default selected index of the period combo box to 0 (Day)
+            comboBox_period.SelectedIndex = 0;
         }
 
         /// <summary>
@@ -34,6 +45,8 @@ namespace StockAnalysisCS
         /// <param name="e"></param>
         private void button_loadTicker_Click(object sender, EventArgs e)
         {
+            // Define the filter for the OpenFileDialog based on the selected symbol and period
+            defineOpenFileDialogFilter();
             // Display the OpenFileDialog to allow the user to select a ticker file
             openFileDialog_loadTicker.ShowDialog();
         }
@@ -45,12 +58,18 @@ namespace StockAnalysisCS
         /// <param name="e"></param>
         private void openFileDialog_loadTicker_FileOk(object sender, CancelEventArgs e)
         {
-            // Set the form Text to the selected file name
-            Text = openFileDialog_loadTicker.FileName;
+            // Set the title of the form to the name of the selected file
+            Text = Path.GetFileNameWithoutExtension(openFileDialog_loadTicker.FileName);
+
+            // Read the candlestick data from the selected file
             readCandlesticksFromFile();
+            // Filter the candlesticks based on the user-selected date range
             filterCandlesticks();
+            // Normalize the y-axis of the chart to fit the candlesticks data
             normalizeChart();
+            // Display the candlestick data in the chart
             displayChart();
+            // Display the candlestick data in the DataGridView
             displayDataGridView();
         }
 
@@ -108,7 +127,7 @@ namespace StockAnalysisCS
 
             // Use LINQ to filter candlesticks within the date range
             // and assign the result to the filteredCandlesticks list
-            filteredCandlesticks = new BindingList<Candlestick>(candlesticks.Where(c => c.date >= startDate && c.date <= endDate).ToList());
+            filteredCandlesticks = candlesticks.Where(c => c.date >= startDate && c.date <= endDate).ToList();
         }
 
         /// <summary>
@@ -151,12 +170,8 @@ namespace StockAnalysisCS
             // Loop through each candlestick in the filtered list
             foreach (var candle in filteredCandlesticks)
             {
-                // Add a data point to the "Series_OHLC" series (X: date, Y: open, high, low, close)
-                int pointIdx = chart_stockData.Series["Series_OHLC"].Points.AddXY(candle.date, candle.open, candle.high, candle.low, candle.close);
-
-                // Set the color for the candlestick
-                // Green for up candlestick (close >= open), Red for down candlestick (close < open)
-                chart_stockData.Series["Series_OHLC"].Points[pointIdx].Color = (candle.close >= candle.open) ? Color.Green : Color.Red;
+                // Add a data point to the "Series_OHLC" series (X: date, Y: high, low, open, close)
+                int pointIdx = chart_stockData.Series["Series_OHLC"].Points.AddXY(candle.date, candle.high, candle.low, candle.open, candle.close);
 
                 // Add a data point to the "Series_Volume" series (X: date, Y: volume)
                 chart_stockData.Series["Series_Volume"].Points.AddXY(candle.date, candle.volume);
@@ -168,8 +183,69 @@ namespace StockAnalysisCS
         /// </summary>
         private void displayDataGridView()
         {
+            // Set the data source of the DataGridView to the filtered candlesticks list
             dataGridView_stockData.DataSource = filteredCandlesticks;
         }
+
+        /// <summary>
+        /// Function to define the filter for the OpenFileDialog based on the selected symbol and period
+        /// </summary>
+        private void defineOpenFileDialogFilter()
+        {
+            // Check if the symbol or period is not selected
+            if (comboBox_symbol.SelectedItem == null || comboBox_period.SelectedItem == null)
+            {
+                // Show an error message
+                MessageBox.Show("Please select a symbol and a period.");
+                // Exit function
+                return;
+            }
+
+            // Get the selected symbol
+            string symbol = comboBox_symbol.SelectedItem.ToString();
+            // Get the selected period
+            string period = comboBox_period.SelectedItem.ToString();
+
+            // Define the period filter based on the selected period
+            string periodFilter;
+
+            // Set the period filter based on the selected period
+            switch (period)
+            {
+                // Case for "Daily" period
+                case "Daily":
+                    // Filter for daily data files
+                    periodFilter = "-Day.csv";
+                    break;
+                // Case for "Weekly" period
+                case "Weekly":
+                    // Filter for weekly data files
+                    periodFilter = "-Week.csv";
+                    break;
+                // Case for "Monthly" period
+                case "Monthly":
+                    // Filter for monthly data files
+                    periodFilter = "-Month.csv";
+                    break;
+                // Default case for "All" period
+                default:
+                    // Filter for data files of all periods
+                    periodFilter = "-*.csv";
+                    break;
+            }
+
+            // Define the file filter based on the selected symbol and period
+            string fileFilter = $"{(symbol == "All" ? "*" : symbol)}{periodFilter}";
+
+            // Define the filtered symbol label based on the selected symbol
+            string symbolLabel = (symbol == "All") ? "All Symbols" : symbol;
+            // Define the filtered period label based on the selected period
+            string periodLabel = (period == "All") ? "All Periods" : period;
+
+            // Set the filter for the OpenFileDialog based on the selected symbol and period
+            openFileDialog_loadTicker.Filter = $"{symbolLabel} - {periodLabel} ({fileFilter})|{fileFilter}";
+        }
+
 
         /// <summary>
         /// Function to handle the event when the user selects a date in the start date picker
@@ -178,8 +254,14 @@ namespace StockAnalysisCS
         /// <param name="e"></param>
         private void dateTimePicker_startDate_ValueChanged(object sender, EventArgs e)
         {
-            // Filter the candlesticks based on the new specified date range
+            // Filter the candlesticks based on the user-selected date range
             filterCandlesticks();
+            // Normalize the y-axis of the chart to fit the candlesticks data
+            normalizeChart();
+            // Display the candlestick data in the chart
+            displayChart();
+            // Display the candlestick data in the DataGridView
+            displayDataGridView();
         }
 
         /// <summary>
@@ -189,8 +271,14 @@ namespace StockAnalysisCS
         /// <param name="e"></param>
         private void dateTimePicker_endDate_ValueChanged(object sender, EventArgs e)
         {
-            // Filter the candlesticks based on the new specified date range
+            // Filter the candlesticks based on the user-selected date range
             filterCandlesticks();
+            // Normalize the y-axis of the chart to fit the candlesticks data
+            normalizeChart();
+            // Display the candlestick data in the chart
+            displayChart();
+            // Display the candlestick data in the DataGridView
+            displayDataGridView();
         }
     }
 }
