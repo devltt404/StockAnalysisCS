cf4bfb5f88dc86e8cd119423f1423ede3a03d5e5
Tri Pham <devltt404@gmail.com>
2025-02-22 20:04:40 -0500

    fa

--- StockAnalysisCS/Form_AnalyzeStock.Designer.cs
+++ StockAnalysisCS/Form_AnalyzeStock.Designer.cs
@@ -50,9 +50,10 @@
             this.button_loadTicker.BackColor = System.Drawing.Color.MediumBlue;
             this.button_loadTicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button_loadTicker.ForeColor = System.Drawing.Color.White;
-            this.button_loadTicker.Location = new System.Drawing.Point(51, 22);
+            this.button_loadTicker.Location = new System.Drawing.Point(34, 14);
+            this.button_loadTicker.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button_loadTicker.Name = "button_loadTicker";
-            this.button_loadTicker.Size = new System.Drawing.Size(202, 69);
+            this.button_loadTicker.Size = new System.Drawing.Size(135, 45);
             this.button_loadTicker.TabIndex = 0;
             this.button_loadTicker.Text = "Load Ticker";
             this.button_loadTicker.UseVisualStyleBackColor = false;
@@ -69,18 +70,20 @@
             // 
             // dateTimePicker_startDate
             // 
-            this.dateTimePicker_startDate.Location = new System.Drawing.Point(146, 115);
+            this.dateTimePicker_startDate.Location = new System.Drawing.Point(97, 75);
+            this.dateTimePicker_startDate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dateTimePicker_startDate.Name = "dateTimePicker_startDate";
-            this.dateTimePicker_startDate.Size = new System.Drawing.Size(272, 26);
+            this.dateTimePicker_startDate.Size = new System.Drawing.Size(183, 20);
             this.dateTimePicker_startDate.TabIndex = 1;
             this.dateTimePicker_startDate.Value = new System.DateTime(2024, 1, 1, 0, 0, 0, 0);
             this.dateTimePicker_startDate.ValueChanged += new System.EventHandler(this.dateTimePicker_startDate_ValueChanged);
             // 
             // dateTimePicker_endDate
             // 
-            this.dateTimePicker_endDate.Location = new System.Drawing.Point(146, 166);
+            this.dateTimePicker_endDate.Location = new System.Drawing.Point(97, 108);
+            this.dateTimePicker_endDate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dateTimePicker_endDate.Name = "dateTimePicker_endDate";
-            this.dateTimePicker_endDate.Size = new System.Drawing.Size(272, 26);
+            this.dateTimePicker_endDate.Size = new System.Drawing.Size(183, 20);
             this.dateTimePicker_endDate.TabIndex = 2;
             this.dateTimePicker_endDate.ValueChanged += new System.EventHandler(this.dateTimePicker_endDate_ValueChanged);
             // 
@@ -89,9 +92,10 @@
             this.label_startDate.AutoSize = true;
             this.label_startDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label_startDate.ForeColor = System.Drawing.Color.Black;
-            this.label_startDate.Location = new System.Drawing.Point(48, 118);
+            this.label_startDate.Location = new System.Drawing.Point(32, 77);
+            this.label_startDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label_startDate.Name = "label_startDate";
-            this.label_startDate.Size = new System.Drawing.Size(91, 22);
+            this.label_startDate.Size = new System.Drawing.Size(61, 15);
             this.label_startDate.TabIndex = 3;
             this.label_startDate.Text = "Start Date";
             // 
@@ -100,9 +104,10 @@
             this.label_endDate.AutoSize = true;
             this.label_endDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label_endDate.ForeColor = System.Drawing.Color.Black;
-            this.label_endDate.Location = new System.Drawing.Point(48, 168);
+            this.label_endDate.Location = new System.Drawing.Point(32, 109);
+            this.label_endDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label_endDate.Name = "label_endDate";
-            this.label_endDate.Size = new System.Drawing.Size(85, 22);
+            this.label_endDate.Size = new System.Drawing.Size(58, 15);
             this.label_endDate.TabIndex = 4;
             this.label_endDate.Text = "End Date";
             // 
@@ -115,7 +120,8 @@
             this.chart_stockData.ChartAreas.Add(chartArea2);
             legend1.Name = "Legend1";
             this.chart_stockData.Legends.Add(legend1);
-            this.chart_stockData.Location = new System.Drawing.Point(52, 302);
+            this.chart_stockData.Location = new System.Drawing.Point(35, 196);
+            this.chart_stockData.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.chart_stockData.Name = "chart_stockData";
             series1.ChartArea = "ChartArea_OHLC";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Candlestick;
@@ -126,7 +132,6 @@
             series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Date;
             series1.YValueMembers = "High,Low,Open,Close";
             series1.YValuesPerPoint = 4;
-            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.UInt64;
             series2.ChartArea = "ChartArea_Volume";
             series2.Legend = "Legend1";
             series2.Name = "Series_Volume";
@@ -136,26 +141,27 @@
             series2.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.UInt64;
             this.chart_stockData.Series.Add(series1);
             this.chart_stockData.Series.Add(series2);
-            this.chart_stockData.Size = new System.Drawing.Size(1526, 417);
+            this.chart_stockData.Size = new System.Drawing.Size(1017, 271);
             this.chart_stockData.TabIndex = 5;
             this.chart_stockData.Text = "chart_stockData";
             // 
             // dataGridView_stockData
             // 
             this.dataGridView_stockData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView_stockData.Location = new System.Drawing.Point(566, 43);
+            this.dataGridView_stockData.Location = new System.Drawing.Point(377, 28);
+            this.dataGridView_stockData.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView_stockData.Name = "dataGridView_stockData";
             this.dataGridView_stockData.ReadOnly = true;
             this.dataGridView_stockData.RowHeadersWidth = 62;
             this.dataGridView_stockData.RowTemplate.Height = 28;
-            this.dataGridView_stockData.Size = new System.Drawing.Size(1012, 231);
+            this.dataGridView_stockData.Size = new System.Drawing.Size(675, 150);
             this.dataGridView_stockData.TabIndex = 6;
             // 
             // Form_AnalyzeStock
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1643, 763);
+            this.ClientSize = new System.Drawing.Size(1095, 496);
             this.Controls.Add(this.dataGridView_stockData);
             this.Controls.Add(this.chart_stockData);
             this.Controls.Add(this.label_endDate);
@@ -163,7 +169,6 @@
             this.Controls.Add(this.dateTimePicker_endDate);
             this.Controls.Add(this.dateTimePicker_startDate);
             this.Controls.Add(this.button_loadTicker);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form_AnalyzeStock";
             this.Text = "Analyze Stock";
             ((System.ComponentModel.ISupportInitialize)(this.chart_stockData)).EndInit();
